5834aa9b411cb65b52554402fa9b40f60a9f4a95
Jordan Rendon C13 <105289424+JordanRendon@users.noreply.github.com>
2022-07-01 18:57:38 -0500

    Update 4_b_sql.sql

--- 4_b_sql.sql
+++ 4_b_sql.sql
@@ -1,15 +1,7 @@
-SELECT id AS 'Id producto', 
-SUM(precio) AS total FROM 
-(SELECT VD_ProdId AS 'Id',
-VD_Precio AS 'Precio', 
-Ventas_Fecha AS 'Fecha venta', 
-Prod_ProvId AS 'Id proveedor'
-FROM ventas_detalle
-JOIN ventas ON Ventas_Id = VD_VentasId
-JOIN productos ON VD_ProdId = Prod_Id
-JOIN proveedores ON Prov_Id = Prod_ProvId
-WHERE Ventas_Fecha
-BETWEEN '2018-01-02' AND '2018-01-10'
-AND Prod_ProvId BETWEEN 2 AND 100
-ORDER BY VD_ProdId) AS T
-GROUP BY id
+SELECT SUM(precio) AS 'Venta total' FROM
+ (SELECT VD_Precio AS precio
+ FROM ventas_detalle
+  JOIN ventas ON VD_VentasId = Ventas_Id
+ WHERE Ventas_Fecha BETWEEN '208-01-02' AND '2018-01-10'
+ AND VD_ProdId BETWEEN 2 AND 100) AS t;
+  
